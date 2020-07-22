6db912ae68f6585bfaadd0bac1107413a9fed9ea
Andrew Moss <drew.moss86@gmail.com>
2020-07-22 14:26:29 -0700

    added xml based config setup

--- src/main/java/guru/springframework/sfgjokesapp/SfgJokesApplication.java
+++ src/main/java/guru/springframework/sfgjokesapp/SfgJokesApplication.java
@@ -2,8 +2,10 @@ package guru.springframework.sfgjokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:ChuckConfig.xml")
 public class SfgJokesApplication {
 
     public static void main(String[] args) {
--- src/main/java/guru/springframework/sfgjokesapp/config/ChuckConfig.java
+++ src/main/java/guru/springframework/sfgjokesapp/config/ChuckConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ChuckConfig {
-    @Bean
+//    @Bean --> commented out to test XML based configuration setup
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
