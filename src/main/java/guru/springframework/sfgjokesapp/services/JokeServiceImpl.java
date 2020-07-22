b8e527aa1e9404d16f90b79a454dfaad982cf752
Andrew Moss <drew.moss86@gmail.com>
2020-07-22 13:36:57 -0700

    refactor jokeserviceimpl using bean

--- src/main/java/guru/springframework/sfgjokesapp/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/sfgjokesapp/services/JokeServiceImpl.java
@@ -1,18 +1,23 @@
 package guru.springframework.sfgjokesapp.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JokeServiceImpl implements JokeService {
 
-    private final ChuckNorrisQuotes cnq;
+    private final ChuckNorrisQuotes cnq; //brought in by external jar
 
-    public JokeServiceImpl() {
-        //instantiating JokeServiceImpl object inside no-arg constructor improves
-        // efficiency...
-        this.cnq = new ChuckNorrisQuotes();
+    //requires bean
+    public JokeServiceImpl(ChuckNorrisQuotes cnq) {
+        this.cnq = cnq;
     }
+//    public JokeServiceImpl() {
+//        //instantiating JokeServiceImpl object inside no-arg constructor improves
+//        // efficiency...
+//        this.cnq = new ChuckNorrisQuotes();
+//    }
 
     @Override
     public String getJoke() {
